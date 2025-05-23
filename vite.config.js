e71acb5b381c931cade686aa0f86309573679846
madmax1109 <jrxshiv@gmail.com>
2025-05-23 20:31:02 +0530

    Deploy application to Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   root: 'handroxx',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: resolve(__dirname, 'handroxx/index.html'),
-      output: {
-        assetFileNames: 'assets/[name][extname]'
-      }
-    }
-  },
-  publicDir: 'public'
+    emptyOutDir: true
+  }
 })
